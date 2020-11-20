54c567713693e84ada0ae7b8cbe134d3a28d1e66
Kunal Dalwadi <kunaldalwadi@gmail.com>
2020-11-20 07:38:53 -0500

    [KD] Added Unbounded Service Demo

---
+++ app/src/main/java/com/example/servicetrial/MyService.java
@@ -0,0 +1,37 @@
+package com.example.servicetrial;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+class MyService extends Service {
+
+    private MediaPlayer mMediaPlayer;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //Things you want service to do goes here.
+        mMediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        mMediaPlayer.setLooping(true);
+        mMediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+
+        //Stop your process or service here.
+        mMediaPlayer.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
